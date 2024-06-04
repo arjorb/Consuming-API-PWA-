2f88b9a35b05d2c56ee5f39ef52aaa4161f8c298
John U <arjorb@gmail.com>
2024-06-04 17:02:54 +0200

    chore: Update CardItem component to fetch and display posts from API

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react"
 
 
 function CardItem() {
-  return (
-    <div>CardItem</div>
-  )
+        const [post, setPost] = useState({}) // Update initial state to empty object
+
+        useEffect(() => {
+                fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(res => res.json())
+                .then(data => setPost(data))
+        }, [])
+        console.log(post)
+    return (
+        <div>
+                <h1>Hello</h1>
+        </div>
+    )
 }
 
 export default CardItem
