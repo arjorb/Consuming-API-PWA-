1949155a8734364fb2c52314106fcc833f294b32
John U <arjorb@gmail.com>
2024-06-05 12:51:29 +0200

    feat: Update service worker caching strategy for API calls and images

--- src/sw.ts
+++ src/sw.ts
@@ -1,37 +1,50 @@
-import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
-import { Route, NavigationRoute, registerRoute,} from 'workbox-routing';
-import { NetworkFirst,NetworkOnly } from 'workbox-strategies';
-import { BackgroundSyncPlugin } from 'workbox-background-sync';
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { Route, NavigationRoute, registerRoute } from "workbox-routing";
+import { NetworkFirst, CacheFirst, NetworkOnly } from "workbox-strategies";
+import { BackgroundSyncPlugin } from "workbox-background-sync";
 
 declare const self: ServiceWorkerGlobalScope;
 
 cleanupOutdatedCaches();
 
-self.skipWaiting();
+precacheAndRoute(self.__WB_MANIFEST);
 
+self.skipWaiting();
 
 // cache api calls
 const fetchPostRoute = new Route(
-    ({request}) => {
-        return request.url === import.meta.env.VITE_BASE_URL;
-    },
-    new NetworkFirst({
-        cacheName: "api/fecth-post",
-    })
+  ({ request }) => {
+    return request.url === import.meta.env.VITE_BASE_URL;
+  },
+  new NetworkFirst({
+    cacheName: "api/fecth-post",
+  })
 );
 registerRoute(fetchPostRoute);
 
+// cache images
+const imageRoute = new Route(
+  ({ request, sameOrigin }) => {
+    return sameOrigin && request.destination === "image";
+  },
+  new CacheFirst({
+    cacheName: "images",
+  })
+);
+
+registerRoute(imageRoute);
+
+//=======================================================================
+
 // cache navigation
-// const navigationRoute = new NavigationRoute(
-//     new NetworkFirst({
-//         cacheName: "navigation",
-//         networkTimeoutSeconds: 3,
-//     })
-// );
+const navigationRoute = new NavigationRoute(
+    new NetworkFirst({
+        cacheName: "navigation",
+        networkTimeoutSeconds: 3,
+    })
+);
 
-// registerRoute(navigationRoute);
-
-
+registerRoute(navigationRoute);
 
 // background sync => Helps to submit our post and patch request when the user is offline and sync it when the user is back online
 
@@ -40,13 +53,25 @@ const bgSyncPlugin = new BackgroundSyncPlugin('backgroundSyncQueue', {
 });
 
 const postSubmitRoute = new Route(
-    ({request}) => {    
-        return request.url === import.meta.env.VITE_BASE_URL + '/posts';
+    ({request}) => {
+        return request.url === import.meta.env.VITE_BASE_URL + '/posts/create';
     },
     new NetworkOnly({
         plugins: [bgSyncPlugin]
     }),
     'POST'
-) 
+)
 
 registerRoute(postSubmitRoute);
+
+const editPostRoute = new Route(
+    ({request}) => {
+        return request.url === import.meta.env.VITE_BASE_URL + '/posts/edit';
+    },
+    new NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'PATCH'
+)
+
+registerRoute(editPostRoute);
