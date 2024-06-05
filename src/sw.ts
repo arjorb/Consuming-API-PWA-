5043801eef7f047d1bc1e74c08234c3682a0f7ba
John U <arjorb@gmail.com>
2024-06-05 12:31:54 +0200

    feat: Add route for caching API calls in service worker

--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,23 @@
 import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
+import { Route, registerRoute} from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
 cleanupOutdatedCaches();
 
-precacheAndRoute(self.__WB_MANIFEST);
-
 self.skipWaiting();
+
+
+// cache api calls
+
+const fetchPostRoute = new Route(
+    ({request}) => {
+        return request.url === import.meta.env.VITE_BASE_URL;
+    },
+    new NetworkFirst({
+        cacheName: "api/fecth-post",
+    })
+);
+
+registerRoute(fetchPostRoute);
