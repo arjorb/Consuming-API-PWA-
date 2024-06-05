80c52740e700edbaecd4b4f5e96d85e2dec67121
John U <arjorb@gmail.com>
2024-06-05 11:42:08 +0200

    feat: Add service worker registration for progressive web app support

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,44 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      injectManifest: {
+        swDest: 'dist/sw.ts',
+      },
+      manifest: {
+        name: 'Recent Posts',
+        short_name: 'Postsio',
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'portrait',
+      },
   })],
 })
