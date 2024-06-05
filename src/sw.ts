4f73e01411eae8dfeec58b812ad40a581e16ea26
John U <arjorb@gmail.com>
2024-06-05 12:44:05 +0200

    chore: Update service worker to include background sync for offline post submission

--- src/sw.ts
+++ src/sw.ts
@@ -1,6 +1,7 @@
 import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
-import { Route, registerRoute} from 'workbox-routing';
-import { NetworkFirst } from 'workbox-strategies';
+import { Route, NavigationRoute, registerRoute,} from 'workbox-routing';
+import { NetworkFirst,NetworkOnly } from 'workbox-strategies';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -10,7 +11,6 @@ self.skipWaiting();
 
 
 // cache api calls
-
 const fetchPostRoute = new Route(
     ({request}) => {
         return request.url === import.meta.env.VITE_BASE_URL;
@@ -19,5 +19,34 @@ const fetchPostRoute = new Route(
         cacheName: "api/fecth-post",
     })
 );
-
 registerRoute(fetchPostRoute);
+
+// cache navigation
+// const navigationRoute = new NavigationRoute(
+//     new NetworkFirst({
+//         cacheName: "navigation",
+//         networkTimeoutSeconds: 3,
+//     })
+// );
+
+// registerRoute(navigationRoute);
+
+
+
+// background sync => Helps to submit our post and patch request when the user is offline and sync it when the user is back online
+
+const bgSyncPlugin = new BackgroundSyncPlugin('backgroundSyncQueue', {
+    maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+});
+
+const postSubmitRoute = new Route(
+    ({request}) => {    
+        return request.url === import.meta.env.VITE_BASE_URL + '/posts';
+    },
+    new NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
+) 
+
+registerRoute(postSubmitRoute);
