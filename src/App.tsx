ab72026c58e7d4cd0a4754f4887eddb3ee593e76
John U <arjorb@gmail.com>
2024-06-06 11:48:04 +0200

    chore: Add react-lazyload package for lazy loading of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import LazyLoad from 'react-lazyload';
 import Title from './components/Title';
 import Card from './components/Card';
 function App() {
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="max-w-7xl mx-auto my-5 p-7">
     <Title/>
+    <LazyLoad height={200} offset={100}>
     <Card/>
+    </LazyLoad>
   </div>
   )
 }
