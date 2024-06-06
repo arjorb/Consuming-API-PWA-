a79442fb25bb8468c857906042f42898bc46a9ab
John U <arjorb@gmail.com>
2024-06-06 11:35:55 +0200

    chore: Update Title component to include welcome message and image

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,11 +4,14 @@ const Title = () => {
       <h1 className="text-2xl text-center font-semibold text-gray-400 my-10">
         Recent Posts
       </h1>
+      <div>
       <img
         src="https://images.unsplash.com/photo-1557992260-ec58e38d363c?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="cover image"
         className="w-full h-80 object-cover rounded-lg"
       />
+      <h2>Welcome to Posts</h2>
+      </div>
     </div>
   );
 };
