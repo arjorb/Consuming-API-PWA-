3d87c790acd24804ded41c553dd180f0962b158c
John U <arjorb@gmail.com>
2024-06-06 14:37:18 +0200

    chore: Update Card component to fetch and display posts from API

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 import CardItem from "./CardItem";
 
 export interface CardProps {
-    id: number;
-    title: string;
-    body: string;
+  id: number;
+  title: string;
+  body: string;
 }
 
 function Card() {
-    const [posts, setPost] = useState([])
+  const [posts, setPost] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await response.json();
-            setPost(data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts`
+      );
+      const data = await response.json();
+      setPost(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const renderPost = posts.map((post:CardProps) => <CardItem key={post.id} post={post} />)
+  const renderPost = posts.map((post: CardProps) => (
+    <CardItem key={post.id} post={post} />
+  ));
   return (
-    <div className="w-full flex flex-wrap justify-between">
-    {renderPost}
-    </div>
+    <div className="w-full flex flex-wrap justify-between">{renderPost}</div>
   );
 }
 
