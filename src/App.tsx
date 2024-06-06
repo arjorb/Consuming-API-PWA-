d12541f493fda43c0a8ae3ac36d3b24f1f708493
John U <arjorb@gmail.com>
2024-06-06 11:51:54 +0200

    chore: Remove react-lazyload package and update Card component to display thumbnail images

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="max-w-7xl mx-auto my-5 p-7">
     <Title/>
-    <LazyLoad height={200} offset={100}>
+
     <Card/>
-    </LazyLoad>
   </div>
   )
 }
