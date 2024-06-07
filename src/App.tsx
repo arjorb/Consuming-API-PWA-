6ffccfc092a38da14b9fc3c849f5a5c200d54cf3
John U <arjorb@gmail.com>
2024-06-07 15:14:21 +0200

    feat: Add NotFound route component for handling 404 errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './routes/Home';
+import NotFound from "./routes/NotFound";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+  },
+  {
+    path: "*",
+    element: <NotFound/>,
   },
 ]);
 
