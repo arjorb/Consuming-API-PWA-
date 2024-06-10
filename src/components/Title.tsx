6733cc7efdec3064871e0a18a21fb1d847a36f7c
John U <arjorb@gmail.com>
2024-06-10 11:11:37 +0200

    refactor: Update Title component to include opacity-80 class for cover image

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,7 +10,7 @@ const Title = () => {
       <img
         src="https://images.unsplash.com/photo-1557992260-ec58e38d363c?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="cover image"
-        className="w-full h-80 object-cover rounded-lg"
+        className="w-full h-80 object-cover rounded-lg opacity-80"
       />
 
     </div>
